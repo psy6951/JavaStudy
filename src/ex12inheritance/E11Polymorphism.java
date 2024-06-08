47ef798a1c0d57285b2cc8337cec08721482405e
박선영 <@DESKTOP-QJN8Q7K>
2024-06-08 22:33:15 +0900

    6/8 test

--- src/ex12inheritance/E11Polymorphism.java
+++ src/ex12inheritance/E11Polymorphism.java
@@ -24,7 +24,7 @@ class Child extends Parent {
 public class E11Polymorphism {
 
 	public static void main(String[] args) {
-		//// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 	}
 
