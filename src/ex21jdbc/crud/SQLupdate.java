c63e890068e00a5a93d9585511400098aa6bba4e
psy6951 <psy6951@gmail.com>
2024-06-28 18:04:12 +0900

    6.28

---
+++ src/ex21jdbc/crud/SQLupdate.java
@@ -0,0 +1,57 @@
+package ex21jdbc.crud;
+
+import java.sql.SQLException;
+
+public class SQLupdate extends MyConnection {
+
+	public SQLupdate(String user, String pass) {
+		super(user, pass);
+	}
+
+	String query;
+	int result;
+	
+	@Override
+	public void dbExecute() {
+		try {
+			//update쿼리문 실행을 위해 Statement 인스턴스 생성
+			stmt = con.createStatement();
+			//Java에서 쿼리문 작성시 예약어는 대문자로 기술하는 것이 가독성이 좋음
+			query = "UPDATE member "
+					+ " SET "
+					+ " pass='0412',"
+					+ " name= '김철수'"
+					+ " regidate=sysdate "
+					+ " WHERE id = 'test1";
+			//update가 적용된 행의 개수가 반환됨
+			result = stmt.executeUpdate(query);
+			System.out.println("[stmt]"+ result + "행 업데이트됨");
+			
+			/////////////////////////////////////////////
+			//인파라미터가 있는 동적 쿼리문 작성
+			query =  "UPDATE member "
+					+" SET name=?, pass=? "
+					+" WHERE id=?";
+			psmt = con.prepareStatement(query);
+			//무한루프로 구성하여 exit를 입력할 때까지는 지속적으로 실행됨
+			while(true) {
+				//인파라미터는  ?의 순서대로 1~3까지 지정하면 된다.
+				psmt.setString(3, inputValue("수정할 아이디"));
+				psmt.setString(1, inputValue("이름"));
+				psmt.setString(2, inputValue("패스워드"));
+				//쿼리문 실행 및 결과 반환
+				result = psmt.executeUpdate();
+				System.out.println("[psmt]"+ result +"행 업데이트됨");
+			}
+		}
+		catch(SQLException e) {
+			System.out.println("쿼리실행 중 예외발생");
+			e.printStackTrace();
+		}
+	}	
+	public static void main(String[] args) {
+		new SQLinsert("study", "1234").dbExecute();
+
+	}
+
+}
